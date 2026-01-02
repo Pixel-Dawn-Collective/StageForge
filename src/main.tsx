35594d9071d7724fe26b034ae652e86a27e9342d
Júlia Alberto <hello@eusoujulia.com.br>
2026-01-02 15:54:17 -0300

    hotfix: removed the Test page from Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Routes from "./routes/routes.tsx";
-import Test from "./pages/Test/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <>

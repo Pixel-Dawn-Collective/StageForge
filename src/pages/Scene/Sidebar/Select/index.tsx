b13aa266acdc488665b0b29bce9a17c953f5f718
Aurora Kruschewsky <auro.kruschewsky@gmail.com>
2026-01-02 23:14:20 -0300

    feat: select files to render in the scene

--- src/pages/Scene/Sidebar/Select/index.tsx
+++ src/pages/Scene/Sidebar/Select/index.tsx
@@ -1,21 +1,55 @@
 import { Repeat } from "feather-icons-react";
 import styles from "./select.module.scss";
+import { type ChangeEvent } from "react";
+import { useSceneStore } from "../../../../utils/storeScene";
+import { useCharacterStore } from "../../../../utils/storeCharacter";
 
 interface SelectProps {
   data: File[];
   title: string;
   hasFlipButton?: boolean;
+  selectType: "scene" | "charLeft" | "charCenter" | "charRight";
 }
 
-const Select: React.FC<SelectProps> = ({ hasFlipButton, title, data }) => {
+const Select: React.FC<SelectProps> = ({
+  selectType,
+  hasFlipButton,
+  title,
+  data,
+}) => {
+  const sceneStore = useSceneStore();
+  const characterStore = useCharacterStore();
+
   const handleData = () => {
     if (data.length === 0) {
       return <option value={0}>Select a folder in the library first</option>;
+    } else if (data.length > 0) {
+      return (
+        <>
+          {data.map((file, i) => (
+            <option key={file.name + i} value={i}>
+              {file.name}
+            </option>
+          ))}
+          ;
+        </>
+      );
     }
+  };
 
-    if (data.length > 0) {
-      data.map((file, i) => <option key={file.name + i} value={i + 1} />);
-    }
+  const handleChangeSelect = (
+    e: ChangeEvent<HTMLSelectElement>,
+    type: string
+  ) => {
+    console.log("Batata entrou");
+    const fileIndex = parseInt(e.target.value);
+    if (type === "scene") sceneStore.setScene(fileIndex);
+
+    if (type === "charLeft") characterStore.setCharacterLeft(fileIndex);
+
+    if (type === "charCenter") characterStore.setCharacterCenter(fileIndex);
+
+    if (type === "charRight") characterStore.setCharacterRight(fileIndex);
   };
 
   return (
@@ -23,7 +57,12 @@ const Select: React.FC<SelectProps> = ({ hasFlipButton, title, data }) => {
       <div className={styles.infoContainer}>
         <p className={styles.selectTitle}>{title}</p>
         <div className={styles.selectWrapper}>
-          <select className={styles.select}>{handleData()}</select>
+          <select
+            className={styles.select}
+            onChange={(e) => handleChangeSelect(e, selectType)}
+          >
+            {handleData()}
+          </select>
           {hasFlipButton && (
             <div className={styles.flipButton}>
               <Repeat />
--- src/pages/Scene/Sidebar/index.tsx
+++ src/pages/Scene/Sidebar/index.tsx
@@ -5,6 +5,8 @@ import Logo from "../../../components/Logo";
 import { useNavigate } from "react-router-dom";
 import TeamLogo from "../../../components/TeamLogo";
 import Select from "./Select";
+import { useSceneStore } from "../../../utils/storeScene";
+import { useCharacterStore } from "../../../utils/storeCharacter";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -20,24 +22,32 @@ const Sidebar = () => {
       <div className={styles.topSidebarContent}>
         <Logo />
         <div className={styles.selectContainer}>
-          <Select title="Scenario" data={[]} key="scenario-select" />
+          <Select
+            key="scenario-select"
+            title="Scenario"
+            data={useSceneStore().getFiles()}
+            selectType="scene"
+          />
           <Select
             key="character-l-select"
             title="Character - Left"
             hasFlipButton
-            data={[]}
+            data={useCharacterStore().getFiles()}
+            selectType="charLeft"
           />
           <Select
             key="character-c-select"
             title="Character - Center"
             hasFlipButton
-            data={[]}
+            data={useCharacterStore().getFiles()}
+            selectType="charCenter"
           />
           <Select
             key="character-r-select"
             title="Character - Right"
             hasFlipButton
-            data={[]}
+            data={useCharacterStore().getFiles()}
+            selectType="charRight"
           />
         </div>
         <div className={styles.btnContainer}>

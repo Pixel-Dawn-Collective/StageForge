21caa50e17c80f2074f6d8553495664a22dc8df0
Aurora Kruschewsky <auro.kruschewsky@gmail.com>
2026-01-02 23:15:46 -0300

    feat: render scene and character images

--- src/pages/Scene/index.tsx
+++ src/pages/Scene/index.tsx
@@ -1,17 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import FlagButton from "./FlagButton";
 import styles from "./scene.module.scss";
 import { AnimatePresence } from "motion/react";
+import { useSceneStore } from "../../utils/storeScene";
+import { useCharacterStore } from "../../utils/storeCharacter";
 
 const Scene = () => {
   const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [scene, setScene] = useState<string>("");
+  const [charLeft, setCharLeft] = useState<string>("");
+  const [charCenter, setCharCenter] = useState<string>("");
+  const [charRight, setCharRight] = useState<string>("");
+
+  const sceneStore = useSceneStore();
+  const characterStore = useCharacterStore();
+
+  useEffect(() => {
+    setScene(sceneStore.getScene());
+  }, [sceneStore]);
+
+  useEffect(() => {
+    setCharLeft(characterStore.getCharacterLeft());
+    setCharCenter(characterStore.getCharacterCenter());
+    setCharRight(characterStore.getCharacterRight());
+  }, [characterStore]);
 
   return (
     <div
       className={styles.background}
+      style={{
+        backgroundImage: `url(${scene})`,
+      }}
       aria-label="Image defined by user, can be changed"
     >
+      <img className={styles.charLeft} src={charLeft} />
+      <img className={styles.charCenter} src={charCenter} />
+      <img className={styles.charRight} src={charRight} />
       <AnimatePresence>{isSidebarOpen && <Sidebar />}</AnimatePresence>
       <FlagButton
         isSidebarOpen={isSidebarOpen}

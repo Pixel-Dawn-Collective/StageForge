4ad5188e1bc1b07e9d4a69732b1bf0421cd42021
Júlia Alberto <hello@eusoujulia.com.br>
2026-01-12 19:05:13 -0300

    style: fix button flag animation

--- src/pages/Scene/FlagButton/index.tsx
+++ src/pages/Scene/FlagButton/index.tsx
@@ -2,7 +2,6 @@ import { type SetStateAction } from "react";
 import styles from "./flagButton.module.scss";
 import { motion } from "motion/react";
 import { Menu, X } from "feather-icons-react";
-import { AnimatePresence } from "motion/react";
 
 interface FlagButtonProps {
   isSidebarOpen: boolean;
@@ -14,14 +13,14 @@ const FlagButton: React.FC<FlagButtonProps> = ({
   setSidebarOpen,
 }) => {
   return (
-    <AnimatePresence>
-      <motion.div
-        className={styles.flagBackground}
-        onClick={() => setSidebarOpen(!isSidebarOpen)}
-      >
-        {isSidebarOpen ? <X color="white" /> : <Menu color="white" />}
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      layout
+      transition={{ ease: "easeInOut", duration: 0.4 }}
+      className={styles.flagBackground}
+      onClick={() => setSidebarOpen(!isSidebarOpen)}
+    >
+      {isSidebarOpen ? <X color="white" /> : <Menu color="white" />}
+    </motion.div>
   );
 };
 
--- src/pages/Scene/Sidebar/index.tsx
+++ src/pages/Scene/Sidebar/index.tsx
@@ -9,6 +9,8 @@ import { useSceneStore } from "../../../utils/storeScene";
 import { useCharacterStore } from "../../../utils/storeCharacter";
 import { useState } from "react";
 
+const isMobile = window.innerWidth <= 768;
+
 interface SidebarProps {
   isSidebarOpen: boolean;
 }
@@ -16,83 +18,122 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen }) => {
   const navigate = useNavigate();
 
-  const [isReset, setIsReset] = useState<boolean>(false);
+  const [resetScene, setResetScene] = useState(false);
+  const [resetCharacters, setResetCharacters] = useState(false);
 
   const sceneStore = useSceneStore();
   const characterStore = useCharacterStore();
 
-  const handleReset = () => {
-    setIsReset(true);
+  const handleResetAll = () => {
+    setResetScene(true);
+    setResetCharacters(true);
     sceneStore.clearScene();
-    characterStore.clearCharacterRight();
-    characterStore.clearCharacterCenter();
     characterStore.clearCharacterLeft();
+    characterStore.clearCharacterCenter();
+    characterStore.clearCharacterRight();
+  };
+
+  const handleResetCharacters = () => {
+    setResetCharacters(true);
+
+    characterStore.clearCharacterLeft();
+    characterStore.clearCharacterCenter();
+    characterStore.clearCharacterRight();
   };
 
   return (
-    <motion.div
-      initial={{ x: "-400px" }}
-      animate={{ x: "0px" }}
-      exit={{ x: "-400px" }}
-      transition={{ ease: "easeInOut", duration: 0.4 }}
+    <motion.aside
+      layout
+      initial={
+        isMobile && isSidebarOpen ? { width: "var(--sidebar-width)" } : false
+      }
       className={styles.sidebarContainer}
+      animate={{ width: isSidebarOpen ? "var(--sidebar-width)" : 0 }}
+      transition={{ type: "spring", stiffness: 300, damping: 30 }}
     >
-      <div className={styles.topSidebarContent}>
-        <Logo />
-        <div className={styles.selectContainer}>
-          <Select
-            key="scenario-select"
-            title="Scenario"
-            data={useSceneStore().getFiles()}
-            selectType="scene"
-            isReset
-            setIsReset={setIsReset}
-            isSidebarOpen={isSidebarOpen}
-          />
-          <Select
-            key="character-l-select"
-            title="Character - Left"
-            hasFlipButton
-            data={useCharacterStore().getFiles()}
-            selectType="charLeft"
-            isReset
-            setIsReset={setIsReset}
-            isSidebarOpen={isSidebarOpen}
-          />
-          <Select
-            key="character-c-select"
-            title="Character - Center"
-            hasFlipButton
-            data={useCharacterStore().getFiles()}
-            selectType="charCenter"
-            isReset
-            setIsReset={setIsReset}
-            isSidebarOpen={isSidebarOpen}
-          />
-          <Select
-            key="character-r-select"
-            title="Character - Right"
-            hasFlipButton
-            data={useCharacterStore().getFiles()}
-            selectType="charRight"
-            isReset={isReset}
-            setIsReset={setIsReset}
-            isSidebarOpen={isSidebarOpen}
-          />
+      <motion.div
+        className={styles.sidebarContent}
+        initial={isMobile && isSidebarOpen ? { opacity: 1 } : false}
+        animate={{ opacity: isSidebarOpen ? 1 : 0 }}
+        transition={{
+          opacity: {
+            duration: 0.2,
+            delay: isSidebarOpen ? 0.2 : 0,
+            ease: "easeInOut",
+          },
+        }}
+        style={{ pointerEvents: isSidebarOpen ? "auto" : "none" }}
+      >
+        <div className={styles.topSidebarContent}>
+          <Logo />
+
+          <div className={styles.selectContainer}>
+            <Select
+              title="Scenario"
+              data={sceneStore.getFiles()}
+              selectType="scene"
+              isReset={resetScene}
+              setIsReset={setResetScene}
+              isSidebarOpen={isSidebarOpen}
+            />
+            <Select
+              title="Character - Left"
+              hasFlipButton
+              data={characterStore.getFiles()}
+              selectType="charLeft"
+              isReset={resetCharacters}
+              setIsReset={setResetCharacters}
+              isSidebarOpen={isSidebarOpen}
+            />
+            <Select
+              title="Character - Center"
+              hasFlipButton
+              data={characterStore.getFiles()}
+              selectType="charCenter"
+              isReset={resetCharacters}
+              setIsReset={setResetCharacters}
+              isSidebarOpen={isSidebarOpen}
+            />
+            <Select
+              title="Character - Right"
+              hasFlipButton
+              data={characterStore.getFiles()}
+              selectType="charRight"
+              isReset={resetCharacters}
+              setIsReset={setResetCharacters}
+              isSidebarOpen={isSidebarOpen}
+            />
+          </div>
+
+          <div className={styles.btnContainer}>
+            <div className={styles.resetWrapper}>
+              <Button
+                appearance="secondary"
+                text="Reset All"
+                onClick={handleResetAll}
+              />
+
+              <Button
+                appearance="secondary"
+                text="Reset Chars"
+                onClick={handleResetCharacters}
+              />
+            </div>
+
+            <Button
+              appearance="primary"
+              text="Back to Home"
+              onClick={() => {
+                handleResetAll();
+                navigate("/");
+              }}
+            />
+          </div>
         </div>
-        <div className={styles.btnContainer}>
-          <Button appearance="primary" text="Reset" onClick={handleReset} />
-          <Button
-            onClick={() => {
-              handleReset(), navigate("/");
-            }}
-            appearance="secondary"
-            text="Back to Home"
-          />
-        </div>
-      </div>
-      <TeamLogo size={{ width: "42px", height: "48px" }} />
-    </motion.div>
+
+        <TeamLogo size={{ width: "42px", height: "48px" }} />
+      </motion.div>
+    </motion.aside>
   );
 };
 
